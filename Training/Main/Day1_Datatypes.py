292be60827aed8252894586d619e6b8d4a4ad563
Mohammed Siddique M <silvally.siddique@gmail.com>
2025-05-07 22:25:06 +0530

    Day1_Datatypes of hackerrank challenge
    Performed Day1 Python code on understanding python datatypes.

---
+++ Training/Main/Day1_Datatypes.py
@@ -0,0 +1,20 @@
+i = 4
+d = 4.0
+s = 'HackerRank'
+# Declare second integer, double, and String variables.
+integer = 5
+decimal = 5.5
+string = 'five'
+# Read and save an integer, double, and String to your variables.
+integer = int(input('Enter the Integer\n'))
+decimal = float(input('Enter the decimal\n'))
+string = input('Enter the String\n')
+
+# Print the sum of both integer variables on a new line.
+print(i+integer)
+
+# Print the sum of the double variables on a new line.
+print(d+decimal)
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+print(s+string)
